bb64bd8704c24404706e43ed02d289f91f65425c
Philip <philip@deranged.dk>
2023-07-22 12:39:05 +0200

    create a container, we can save stuff in

---
+++ src/models.ts
@@ -0,0 +1,6 @@
+export const container = {
+    id: "123lkjasdfl",
+    name: "Box 1",
+    contains: [],
+    add: (obj: any) => this.contains.push(obj)
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import express from "express";
 const app = express();
 const port = 5000;
+import { container } from "./models";
 
 app.get("/health", (req, res) => {
   res.send("All good");
 });
 
 app.post("/item", (req, res) => {
-  const item = req.body;
+  const item: any = req.body;
   if(typeof item.name === "undefined") {
     res.sendStatus(400);
   }
+
+  container.add(item);
+  res.sendStatus(200);
 })
 
 app.listen(port, () => {
