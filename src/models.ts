3c7ef0e5e20333e754f7637914d4db338938a0f6
Philip <philip@deranged.dk>
2023-07-27 22:33:19 +0200

    change id to string

--- src/models.ts
+++ src/models.ts
@@ -2,6 +2,6 @@
 
 export const itemsDb = {
     db: [],
-    add: (item: {id: number, name: string }) => itemsDb.db.push(item),
+    add: (item: {id: string, name: string }) => itemsDb.db.push(item),
     list: () => itemsDb.db
 }
